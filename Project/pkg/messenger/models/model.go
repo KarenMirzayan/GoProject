a494311241dbdd02a654f4c6acbaab92d3061bac
Karen Mirzayan <k_mirzayan@kbtu.kz>
2024-04-14 13:38:47 +0500

    User model

--- Project/pkg/messenger/models/model.go
+++ Project/pkg/messenger/models/model.go
@@ -16,7 +16,7 @@ var (
 )
 
 type Models struct {
-	Users         UsersModel
+	Users         UserModel
 	Conversations ConversationsModel
 	Messages      MessagesModel
 }
@@ -25,7 +25,7 @@ func NewModels(db *sql.DB) Models {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 	return Models{
-		Users: UsersModel{
+		Users: UserModel{
 			DB:       db,
 			InfoLog:  infoLog,
 			ErrorLog: errorLog,
